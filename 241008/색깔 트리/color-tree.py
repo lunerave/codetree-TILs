59bf1a6595101eda688585b66db7fe43bdf0dd1e
Sangrok Lee 이상록 <lunerave07@naver.com>
2024-10-08 02:36:57 +0900

    Title: 색깔 트리, Time: 3763ms, Memory: 45MB, Status: Passed - CodeTree

--- 241008/색깔 트리/color-tree.py
+++ 241008/색깔 트리/color-tree.py
@@ -1,101 +1,98 @@
-MAX_ID = 100001
-MAX_DEPTH = 101
-COLOR_MAX = 5
-
-isRoot = [False] * MAX_ID
-
+from collections import defaultdict, deque
+import sys
+input = sys.stdin.readline
 class Node:
-    def __init__(self):
-        self.id = 0
-        self.color = 0
-        self.lastUpdate = 0
-        self.maxDepth = 0
-        self.parentId = 0
-        self.childIds = []
+    def __init__(self, m_id, p_id, color, max_depth):
+        self.m_id = m_id
+        self.p_id = p_id
+        self.color = color
+        self.max_depth = max_depth
+        self.parent_node = None
+        self.color_value_list = [False] * 5
+        self.child_node_list = []
 
-class ColorCount:
-    def __init__(self):
-        self.cnt = [0] * (COLOR_MAX + 1)
+    def check_parent_node(self, p_id, depth_count=2):
+        if p_id == -1:
+            return True
+        if id_node_check_list[p_id].max_depth < depth_count:
+            return False
+        else:
+            next_node = id_node_check_list[p_id]
+            return next_node.check_parent_node(next_node.p_id, depth_count + 1)
     
-    def __add__(self, obj):
-        res = ColorCount()
-        for i in range(1, COLOR_MAX + 1):
-            res.cnt[i] = self.cnt[i] + obj.cnt[i]
-        return res
+    def add_parent_node(self, p_id):
+        if self.check_parent_node(p_id):
+            self.p_id = p_id
+            self.parent_node = id_node_check_list[p_id]
+            self.parent_node.child_node_list.append(self)
+            return True
+            
+
+    def reset_color_value_list(self):
+        self.color_value_list = [False] * 5
+
+
+def dfs_color_update(node):
+    """DFS로 서브트리 내의 색상 정보를 갱신"""
+    node.color_value_list = [False] * 5
+    node.color_value_list[node.color - 1] = True
+
+    for child in node.child_node_list:
+        child_colors = dfs_color_update(child)
+        for i in range(5):
+            if child_colors[i]:
+                node.color_value_list[i] = True
     
-    def score(self):
-        result = 0
-        for i in range(1, COLOR_MAX + 1):
-            result += 1 if self.cnt[i] else 0 
+    return node.color_value_list
+
+
+def calculate_score():
+    """서브트리 내의 색상을 계산하고 점수를 구하는 함수"""
+    total_sum = 0
+    
+    # 트리의 루트 노드들부터 시작해서 DFS로 색상 정보 갱신
+    for node in id_node_check_list.values():
+        if node.p_id == -1:  # 루트 노드에서만 시작
+            dfs_color_update(node)
+
+    # 각 노드별로 서브트리의 색상 수를 세고 점수를 계산
+    for node in id_node_check_list.values():
+        unique_colors = sum(node.color_value_list)
+        total_sum += unique_colors * unique_colors
+    
+    print(total_sum)
+
+
+n = int(input())
+id_node_check_list = defaultdict(Node)
+
+for _ in range(n):
+    command, *args = map(int, input().split())
+    
+    if command == 100:
+        m_id, p_id, color, max_depth = args
+        node = Node(m_id, p_id, color, max_depth)
         
-        return result * result
-
-nodes = [Node() for _ in range(MAX_ID)]
-
-def canMakeChild(curr, needDepth):
-    if curr.id == 0:
-        return True
-    if curr.maxDepth <= needDepth:
-        return False
+        if p_id == -1:
+            id_node_check_list[m_id] = node
+        else:
+            if node.add_parent_node(p_id):
+                id_node_check_list[m_id] = node
     
-    return canMakeChild(nodes[curr.parentId], needDepth + 1)
-
-def getColor(curr):
-    if curr.id == 0:
-        return 0, 0
-    info = getColor(nodes[curr.parentId])
-    if info[1] > curr.lastUpdate:
-        return info
-    else:
-        return curr.color, curr.lastUpdate
-
-def getBeauty(curr, color, lastUpdate):
-    if lastUpdate < curr.lastUpdate:
-        lastUpdate = curr.lastUpdate
-        color = curr.color
-    result = [0, ColorCount()]
-    result[1].cnt[color] = 1
-
-    for childId in curr.childIds:
-        child = nodes[childId]
-        subResult = getBeauty(child, color, lastUpdate)
-        result[1] = result[1] + subResult[1]
-        result[0] += subResult[0]
-    result[0] += result[1].score()
-    return result
-
-
-if __name__ == "__main__":
-    Q = int(input())
-
-    for i in range(1, Q+1):
-        query = list(map(int, input().split()))
-
-        T = query[0]
-
-        if T == 100:
-            mId, pId, color, maxDepth = query[1:]
-            if pId == -1:
-                isRoot[mId] = True
-            if isRoot[mId] or canMakeChild(nodes[pId], 1):
-                nodes[mId].id = mId
-                nodes[mId].color = color
-                nodes[mId].maxDepth = maxDepth
-                nodes[mId].parentId = 0 if isRoot[mId] else pId
-                nodes[mId].lastUpdate = i
-
-                if not isRoot[mId]:
-                    nodes[pId].childIds.append(mId)
-        elif T == 200:
-            mId, color = query[1:]
-            nodes[mId].color = color
-            nodes[mId].lastUpdate = i
-        elif T == 300:
-            mId = query[1]
-            print(getColor(nodes[mId])[0])
-        elif T == 400:
-            beauty = 0
-            for i in range(1, MAX_ID):
-                if isRoot[i]:
-                    beauty += getBeauty(nodes[i], nodes[i].color, nodes[i].lastUpdate)[0]
-            print(beauty)
+    elif command == 200:
+        m_id, new_color = args
+        node = id_node_check_list[m_id]
+        q = deque([node])
+        
+        while q:
+            current_node = q.popleft()
+            current_node.color = new_color
+            for child in current_node.child_node_list:
+                q.append(child)
+    
+    elif command == 300:
+        m_id = args[0]
+        print(id_node_check_list[m_id].color)
+    
+    elif command == 400:
+        calculate_score()
